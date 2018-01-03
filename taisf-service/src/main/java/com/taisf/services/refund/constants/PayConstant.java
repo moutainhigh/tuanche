e43d3c0d9ee5ec9ad69fc58e8aa08a3b71327847
dingwf2 <dingwf2@ziroom.com>
2018-01-03 14:06:43 +0800

    退款的逻辑

--- taisf-service/src/main/java/com/taisf/services/refund/constants/PayConstant.java
+++ taisf-service/src/main/java/com/taisf/services/refund/constants/PayConstant.java
@@ -5,19 +5,19 @@ import org.springframework.stereotype.Component;
 @Component("pay.payConstant")
 public class PayConstant {
 
-	@Value("#{'${PAY_KEY}'}")
+	@Value("#{'${PAY_KEY:}'}")
 	public String PAY_KEY;
 
 
-	@Value("#{'${pay_api_url}'}")
+	@Value("#{'${pay_api_url:}'}")
 	public String PAY_API_URL;
 
 
-	@Value("#{'${product_code_ask}'}")
+	@Value("#{'${product_code_ask:}'}")
 	public String PRODUCT_CODE_ASK;
 
 
-	@Value("#{'${refund_platform_url}'}")
+	@Value("#{'${refund_platform_url:}'}")
 	public String REFUND_PLATFORM_URL;
 
 
