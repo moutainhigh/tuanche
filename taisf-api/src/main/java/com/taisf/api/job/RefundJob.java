92625d6b995c196b9bcc47c6cf8d2646d179106f
dingwf2 <dingwf2@ziroom.com>
2018-01-03 11:26:40 +0800

    退款的逻辑

---
+++ taisf-api/src/main/java/com/taisf/api/job/RefundJob.java
@@ -0,0 +1,46 @@
+package com.taisf.api.job;
+
+import com.jk.framework.log.utils.LogUtil;
+import com.taisf.services.refund.job.CreateRefundJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import javax.annotation.Resource;
+
+/**
+ * <p>TODO</p>
+ * <p/>
+ * <PRE>
+ * <BR>	修改记录
+ * <BR>-----------------------------------------------
+ * <BR>	修改日期			修改人			修改内容
+ * </PRE>
+ *
+ * @author afi on on 2018/1/3.
+ * @version 1.0
+ * @since 1.0
+ */
+public class RefundJob {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RefundJob.class);
+
+    @Resource(name = "refund.createRefundJob")
+    private CreateRefundJob createRefundJob;
+
+
+    @Scheduled(cron="0 0/1 * * * ?")
+    public void createRefund() {
+        LogUtil.info(LOGGER,"创建退款 开始执行");
+        createRefundJob.createRefund(null);
+        LogUtil.info(LOGGER,"创建退款 执行结束");
+    }
+
+    @Scheduled(cron="0 0/1 * * * ?")
+    public void breakRefund() {
+        LogUtil.info(LOGGER,"失败发起退款 开始执行");
+        createRefundJob.breakRefund(null);
+        LogUtil.info(LOGGER,"失败发起退款 执行结束");
+    }
+
+}
