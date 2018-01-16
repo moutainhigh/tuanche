d99a1d12eef2feafe2606b734c6ca915885d5220
dingwf2 <dingwf2@ziroom.com>
2018-01-16 10:45:09 +0800

    处理企业信息

--- taisf-service/src/main/java/com/taisf/services/enterprise/dao/EnterpriseDao.java
+++ taisf-service/src/main/java/com/taisf/services/enterprise/dao/EnterpriseDao.java
@@ -30,7 +30,7 @@ import java.util.List;
 @Repository("enterprise.enterpriseDao")
 public class EnterpriseDao extends BaseDao {
 
-    private String SQLID = "oms.enterpriseDao.";
+    private String SQLID = "enterprise.enterpriseDao.";
 
     /**
      * 日志对象
